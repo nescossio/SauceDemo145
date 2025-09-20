bb6f7acd23f049bff1baf96d28b23dca2bda1fe5
Nathalia Cavalcante <nathalia.escossio@gmail.com>
2025-09-20 10:22:20 -0300

    Refatorando códigos de testes

--- SauceDemo145/tests/blazedemo_login.spec.js
+++ SauceDemo145/tests/blazedemo_login.spec.js
@@ -2,16 +2,19 @@ const { test, expect } = require('@playwright/test')
 
 test('fazer_login', async({page}) => {
     await page.goto('https://www.blazedemo.com')
-    await expect(page).toHaveURL('https://www.blazedemo.com')
+    await expect(page).toHaveURL('https://www.blazedemo.com') // Valida URL
     await expect(page.locator('h1')).toHaveText('Welcome to the Simple Travel Agency!')
 
-    const section_home = page.getByRole('link', { name: 'home' });
+    // Criando constante para a sessão home
+    const section_home = page.getByRole('link', { name: 'home' }) 
     await expect(section_home).toHaveText('home')
     await section_home.click()
 
+    // Valida redirecionamento para a sessão home
     await expect(page).toHaveURL('https://www.blazedemo.com/login')
     await expect(page.locator('.panel-heading')).toHaveText('Login')
 
+    // Preenche dados do usuário para login
     await page.locator('#email').fill('nathalia@email')
     await page.locator('#password').fill('123456')
 
@@ -25,9 +28,9 @@ test('fazer_login', async({page}) => {
 
     const botao_login = page.locator('.btn.btn-primary')
     await expect(botao_login).toHaveText('Login')
-    await botao_login.click()
+    await botao_login.click() // Clicando no botão de login
 
-    await expect(page.locator('.message')).toHaveText('Page Expired')
+    await expect(page.locator('.message')).toHaveText('Page Expired') // Validando mensagem final
 
 })
 
